1d4caf386abd02b677e35cf6200e0aecc284b601
Sanjay Kumar <Sanjay.Kumar@emishealth.com>
2025-08-06 00:00:30 +0530

    test5

---
+++ tests/pageObjects/ProductPage.ts
@@ -0,0 +1,20 @@
+import { Page } from "playwright";
+import config from "../config";
+import { expect } from "@playwright/test";
+
+class ProductPage {
+    page: Page
+    productsLogo: string;
+
+    constructor(page : Page){
+        this.page = page;
+        this.productsLogo = "Products";
+    }
+  
+    async isProductPage(){
+        const locator = this.page.getByText(this.productsLogo);
+        await expect(locator).toBeVisible();
+    }
+}
+
+export { ProductPage };
